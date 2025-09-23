f37ea05b196bd219bcb994cd3620c1e3d74be41c
Ankit <ankitbembalgi1405@gmail.com>
2025-09-23 22:24:10 +0530

    uv

--- Year_2/DSA/Unit_1/cdll.c
+++ Year_2/DSA/Unit_1/cdll.c
@@ -32,12 +32,6 @@ void traverse(struct Node* head) {
     printf("\n");
 }
 
-int main() {
-    struct Node* head = NULL;
-    insert_end(&head, 10); insert_end(&head, 20); insert_end(&head, 30);
-    traverse(head);
-    return 0;
-}
 void delete_node(struct Node** head, int key) {
     if (!*head) return;
     struct Node *curr = *head, *prev = NULL;
@@ -57,4 +51,67 @@ void delete_node(struct Node** head, int key) {
         prev = curr;
         curr = curr->next;
     } while (curr != *head);
+}
+
+insert_at_position(struct Node** head, int val, int pos) {
+    struct Node* newnode = malloc(sizeof(struct Node));
+    newnode->data = val;
+    if (pos == 0) {
+        if (!(*head)) {
+            newnode->next = newnode->prev = newnode;
+            *head = newnode;
+            return;
+        }
+        struct Node* last = (*head)->prev;
+        newnode->next = *head;
+        (*head)->prev = newnode;
+        newnode->prev = last;
+        last->next = newnode;
+        *head = newnode;
+        return;
+    }
+    struct Node* temp = *head;
+    for (int i = 0; i < pos - 1; i++) {
+        temp = temp->next;
+        if (temp == *head) break; // position is out of bounds
+    }
+    newnode->next = temp->next;
+    newnode->prev = temp;
+    temp->next->prev = newnode;
+    temp->next = newnode;
+}
+
+int main() {
+    struct Node* head = NULL;
+    int choice, val, pos;
+    while (1) {
+        printf("1. Insert at end\n2. Insert at position\n3. Delete node\n4. Traverse\n5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &val);
+                insert_end(&head, val);
+                break;
+            case 2:
+                printf("Enter value and position to insert: ");
+                scanf("%d %d", &val, &pos);
+                insert_at_position(&head, val, pos);
+                break;
+            case 3:
+                printf("Enter value to delete: ");
+                scanf("%d", &val);
+                delete_node(&head, val);
+                break;
+            case 4:
+                traverse(head);
+                break;
+            case 5:
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+    return 0;
 }
